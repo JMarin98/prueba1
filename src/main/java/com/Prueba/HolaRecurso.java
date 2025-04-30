c16912c239381376c107540c547578c5495f5956
Jeison Marin <jesonmm@hotmail.com>
2025-04-30 17:22:12 -0500

    creacion respuesta "mundo"

--- src/main/java/com/Prueba/HolaRecurso.java
+++ src/main/java/com/Prueba/HolaRecurso.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/C:/Program Files/Git/hola")
+@Path("/mundo")
 public class HolaRecurso {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String convertir() {
+        return "mundo";
     }
 }
