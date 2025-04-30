e99d753fbc42239ef1cafe4dcce897c1010adb71
Jeison Marin <jesonmm@hotmail.com>
2025-04-30 17:00:19 -0500

    Proyecto inicial Quarkus con Java 17

---
+++ src/test/java/com/Prueba/HolaRecursoIT.java
@@ -0,0 +1,8 @@
+package com.Prueba;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HolaRecursoIT extends HolaRecursoTest {
+    // Execute the same tests but in packaged mode.
+}
